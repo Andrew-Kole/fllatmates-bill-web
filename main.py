44eaab0eb819455891ed0424ea8f624a885728d6
pakitowka <whitesaint12@gmail.com>
2023-08-02 22:50:53 +0200

    Home page

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
 class HomePage(MethodView):
-    pass
+    """Represents the main page of my app."""
+
+    def get(self):
+        return render_template('index.html')
 
 
 class BillFormPage(MethodView):
-    pass
+    """Represents the form for bill data filling."""
+
+    def get(self):
+        return "I am a bill form page!"
 
 
 class ResultPage(MethodView):
+    """Represents a page with results of app work."""
+
     pass
 
 
 class BillForm(Form):
     pass
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run(debug=True)
