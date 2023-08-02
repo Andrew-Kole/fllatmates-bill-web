912bcb0b015677c7835731e33a9b970845e21fd0
pakitowka <whitesaint12@gmail.com>
2023-08-02 23:19:32 +0200

    web form implementing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from wtforms import Form
+from wtforms import Form, StringField, SubmitField
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -16,7 +16,8 @@ class BillFormPage(MethodView):
     """Represents the form for bill data filling."""
 
     def get(self):
-        return "I am a bill form page!"
+        bill_form = BillForm()
+        return render_template('bill_form_page.html', bill_form=bill_form)
 
 
 class ResultPage(MethodView):
@@ -26,7 +27,18 @@ class ResultPage(MethodView):
 
 
 class BillForm(Form):
-    pass
+    """The submitting form used by BillFormPage and bill_form_page for jinjas"""
+    # General info about a bill
+    amount = StringField(label="Bill Amount: ")
+    period = StringField(label="Bill Period: ")
+    # Info about first flatmate
+    name1 = StringField(label="Name: ")
+    days_in_house1 = StringField(label="Days in the house: ")
+    # Info about second flatmate
+    name2 = StringField(label="Name: ")
+    days_in_house2 = StringField(label="Days in the house: ")
+
+    submit_button = SubmitField("Calculate")
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
